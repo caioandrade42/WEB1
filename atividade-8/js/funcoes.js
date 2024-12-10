3a0ab49a94457bedcbf9cd8f80c146db1728aa7b
caioandrade42 <caioandradesje@gmail.com>
2024-12-10 17:20:30 -0300

    atividade 8 completa

--- atividade-8/js/funcoes.js
+++ atividade-8/js/funcoes.js
@@ -1,16 +1,14 @@
-
-
 function someCarta(elem){
-
-  if(elem.src !== "img/Carta-Costas.pgn"){
-      elem.src = "img/Carta-Costas.pgn";
+  if(elem.src !== "img/traseira1.jpg"){
+      elem.src = "img/traseira1.jpg";
   }
 }
 
 function poeCarta(objArray,cartaBaixo) {
   for (let index = 0; index < objArray.length; index++) {
-    if (objArray[index].src === "") {
-      objArray[index].src = cartaBaixo.src;      
+    if (objArray[index].src.endsWith("traseira1.jpg")) {
+      objArray[index].src = cartaBaixo.src;
+      return;      
     }
   }
 }
--- atividade-8/js/modulo.js
+++ atividade-8/js/modulo.js
@@ -1,19 +1,71 @@
 import { someCarta, poeCarta } from "./funcoes.js";
 
 
-let cartaCima1 = 
-let cartaCima2 = 
-let cartaCima3 =
-let cartaBaixo01 = 
-let cartaBaixo02 = 
-let cartaBaixo03 =
-let cartaBaixo04 =
-let cartaBaixo05 =
-let cartaBaixo06 =
-let cartaBaixo07 =
-let cartaBaixo08 =
-let cartaBaixo09 =
-let cartaBaixo10 =
+const cartaCima1 = document.getElementById('cartaCima01');
+const cartaCima2 = document.getElementById('cartaCima02');
+const cartaCima3 = document.getElementById('cartaCima03');
+const cartaBaixo01 = document.getElementById('cartaBaixo01');
+const cartaBaixo02 = document.getElementById('cartaBaixo02');
+const cartaBaixo03 = document.getElementById('cartaBaixo03');
+const cartaBaixo04 = document.getElementById('cartaBaixo04');
+const cartaBaixo05 = document.getElementById('cartaBaixo05');
+const cartaBaixo07 = document.getElementById('cartaBaixo06');
+const cartaBaixo06 = document.getElementById('cartaBaixo07');
+const cartaBaixo08 = document.getElementById('cartaBaixo08');
+const cartaBaixo09 = document.getElementById('cartaBaixo09');
+const cartaBaixo10 = document.getElementById('cartaBaixo10');
+
 
 let objArray = [cartaCima1,cartaCima2,cartaCima3];
 
+cartaCima1.addEventListener("click", ()=>{
+  someCarta(cartaCima1)
+});
+
+cartaCima2.addEventListener("click", ()=>{
+  someCarta(cartaCima2)
+});
+
+cartaCima3.addEventListener("click", ()=>{
+  someCarta(cartaCima3)
+});
+
+cartaBaixo01.addEventListener("click", () => {
+  poeCarta(objArray,cartaBaixo01);
+});
+
+cartaBaixo02.addEventListener("click", () => {
+  poeCarta(objArray, cartaBaixo02);
+});
+
+cartaBaixo03.addEventListener("click", () => {
+  poeCarta(objArray, cartaBaixo03);
+});
+
+cartaBaixo04.addEventListener("click", () => {
+  poeCarta(objArray, cartaBaixo04);
+});
+
+cartaBaixo05.addEventListener("click", () => {
+  poeCarta(objArray, cartaBaixo05);
+});
+
+cartaBaixo06.addEventListener("click", () => {
+  poeCarta(objArray, cartaBaixo06);
+});
+
+cartaBaixo07.addEventListener("click", () => {
+  poeCarta(objArray, cartaBaixo07);
+});
+
+cartaBaixo08.addEventListener("click", () => {
+  poeCarta(objArray, cartaBaixo08);
+});
+
+cartaBaixo09.addEventListener("click", () => {
+  poeCarta(objArray, cartaBaixo09);
+});
+
+cartaBaixo10.addEventListener("click", () => {
+  poeCarta(objArray, cartaBaixo10);
+});
