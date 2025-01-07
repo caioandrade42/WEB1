219de57f5a237aff769b437df60aa657f0035387
CAIO CEZAR SALOMAO ANDRADE <0072322@IFMG.EDU.BR>
2025-01-07 09:50:45 -0300

    aula 07-01

---
+++ 07-01/js/script.js
@@ -0,0 +1,43 @@
+
+
+function getMunicipios(uf) {
+  const cidade = document.getElementById('cidade');
+  fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`)
+  .then(response=>response.json())
+  .then(response=>{
+    console.log(response);
+    for (let i = 0; i < response.length; i++) {
+      const option = document.createElement('option');
+      option.innerText = response[i].nome;
+      option.value = response[i].nome;
+      option.id = response[i].id;
+      cidade.appendChild(option);      
+    }
+  })
+}
+
+const awaitMunicipios = async (uf) => {
+  const res = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`);
+  const data = await res.json();
+  console.log(data);
+    for (let i = 0; i < data.length; i++) {
+      const option = document.createElement('option');
+      option.innerText = data[i].nome;
+      option.value = data[i].nome;
+      option.id = data[i].id;
+      cidade.appendChild(option);  
+    }
+}
+
+const uf = document.querySelector('#estado');
+const salvar = document.getElementById('salvar');
+uf.addEventListener('input', function (e){
+  console.log(cidade.length);
+  for (let i = 0; i < cidade.length; i++) {
+    cidade.remove(i);
+  }
+  e.preventDefault();
+  if(uf.innerText.length === 2){
+    awaitMunicipios(uf.value);
+  }
+});
