4aaa3997f9b4a5a0e43d21512eee83d66b46ecb4
CAIO CEZAR SALOMAO ANDRADE <0072322@IFMG.EDU.BR>
2025-01-13 08:25:31 -0300

    aula 13/01/25

---
+++ 13-01/funcoes.php
@@ -0,0 +1,13 @@
+<?php
+    function funcao1($msg){
+        echo 'Mensagem '.$msg.'<br>';
+    }
+
+    funcao1('Gugu lendario');
+
+    $f2 = function(){
+        echo 'Mensagem da segunda funcao irru<br>';
+    };
+
+
+
---
+++ 13-01/repeticao.php
@@ -0,0 +1,10 @@
+<?php
+    for ($i = 1; $i <= 10; $i++) {
+        echo "$i: " . $i . "<br>";
+    }
+
+    echo '<h3>Com While?<h3>';
+    $i=20;
+    while ($i<=20) {
+        echo "$i: " . $i**2 . "<br>";
+    }
